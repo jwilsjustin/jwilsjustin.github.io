af0d8f1bab46fc25a540d0e4786e5a37971fc4e6
Justin Wilson <justin@jwils.co>
2012-11-17 17:53:21 -0500

    rinku auto_link

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'pg'
 gem 'json'
 gem 'haml'
 gem 'twitter'
+gem 'rinku', '~> 1.2.2', :require => 'rails_rinku'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
-  
+
+  require 'rails_rinku'
+
   def index
-    @tweet = Twitter.user_timeline("jwilsjustin").first.text
+    @twitter_user = Twitter.user("jwilsjustin")
+    if @twitter_user
+      @tweet = view_context.auto_link(@twitter_user.status.text).html_safe
+    else
+      @tweet = "(nothing found)"
+    end
   end
 
 end
