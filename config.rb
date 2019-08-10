6f1e8883f836046b138316853b4a62ba107a1ad3
Justin Wilson <justin@jwils.co>
2019-08-10 14:54:34 -0400

    Getting it all working :smile:

--- config.rb
+++ config.rb
@@ -1,9 +1,15 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
-activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
-end
+activate :external_pipeline,
+  name: :webpack,
+  command: build? ? 'yarn run build' : 'yarn run start',
+  source: 'dist',
+  latency: 1
+
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascript'
+set :images_dir, 'images'
 
 # Layouts
 # https://middlemanapp.com/basics/layouts/
