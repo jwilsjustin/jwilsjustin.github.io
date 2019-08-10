4cac7f3951308e43aa01da16a7ca5c5909b043f4
Justin Wilson <justin@jwils.co>
2019-08-10 15:18:57 -0400

    update fancy javascript on contact page

--- source/assets/javascript/index.js
+++ source/assets/javascript/index.js
@@ -1,1 +1,12 @@
 console.log('hello world')
+
+//var username = "justin";
+//var tld = ".co"
+//var linktext = username + "&#064;" + hostname + tld;
+var lastfour = "9433"
+var last = "il"
+$(document).ready(function() {
+  //$('td h2#themessagesystem').html('<a href="mailto:' + linktext + '?Subject=Hello">' + linktext + '</a>')
+  $('td h2#contactdigits').html('<a href="tel:864787' + lastfour + '">864-787-' + lastfour + '</a>')
+  $('td h2#themessagesystemlabel').html('<small style="text-transform:capitalize">ema'+ last +'</small>')
+});
