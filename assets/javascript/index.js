8ad754a2e9d1b390dc8eb364711b02368d7a46b3
Justin Wilson <justin@moonclerk.com>
2020-11-15 12:36:14 -0500

    Automated commit at 2020-11-15 17:36:14 UTC by middleman-deploy 2.0.0-alpha

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,12 +1,9 @@
-console.log('hello world')
+'use strict';
 
-//var username = "justin";
-//var tld = ".co"
-//var linktext = username + "&#064;" + hostname + tld;
-var lastfour = "9433"
-var last = "il"
-$(document).ready(function() {
-  //$('td h2#themessagesystem').html('<a href="mailto:' + linktext + '?Subject=Hello">' + linktext + '</a>')
-  $('td h2#contactdigits').html('<a href="tel:864787' + lastfour + '">864-787-' + lastfour + '</a>')
-  $('td h2#themessagesystemlabel').html('<small style="text-transform:capitalize">ema'+ last +'</small>')
-});
+(() => {
+  const last = 'il'
+  const label = document.getElementById('themessagesystemlabel')
+  if (label !== null) {
+    label.innerHTML = '<small style="text-transform:capitalize">ema' + last + '</small>'
+  }
+}).call(window)
