f106788e5282eb05dff9910b28dcdc1028a244a2
Justin Wilson <justin@moonclerk.com>
2023-10-01 15:10:09 -0400

    convert everything to erb

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
 content: ["./source/**/*.{html,slim,erb}"],
   theme: {
-    extend: {},
-    fontFamily: {
-      'serif': ['"Gentium Book Basic"', 'Palatino', 'Georgia', 'serif']
-    }
+    extend: {
+      fontFamily: {
+        'serif': ['"Gentium Book Basic"', 'Palatino', 'Georgia', 'serif']
+      }
+    },
   },
   plugins: [],
 }
